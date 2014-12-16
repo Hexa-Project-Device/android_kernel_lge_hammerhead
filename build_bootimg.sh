402e91be843b56828a8be48496eaa005934b0df4
KangDroid <KangDroid@KangDroidui-MacBook-Air.local>
2014-12-16 16:53:23 +0000

    Before we go, I think we should clean output boot.img first.

--- build_bootimg.sh
+++ build_bootimg.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+#Clean!
+rm -rf boot.img
+
+#Build!
 mkbootimg --kernel arch/arm/boot/zImage-dtb --ramdisk ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --output boot.img
