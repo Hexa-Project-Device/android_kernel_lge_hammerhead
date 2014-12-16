2be1d34d5ba930597c723ddc466387a07acf3f4f
KangDroid <KangDroid@KangDroidui-MacBook-Air.local>
2014-12-16 16:52:19 +0000

    Add build boot image script

---
+++ build_bootimg.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mkbootimg --kernel arch/arm/boot/zImage-dtb --ramdisk ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --output boot.img
