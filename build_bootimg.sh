b5cc29156f5feb128f888e33f6726af6ef6f8b8a
KangDroid <KangDroid@KangDroidui-MacBook-Air.local>
2014-12-16 16:58:11 +0000

    Big update

--- build_bootimg.sh
+++ build_bootimg.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+#WARNING: This script is ONLY Modified for Nexus5. Ramdisk, Anything included in this command is based on Hammerhead(Nexus5).
+#Also, This script is based on LOLLIPOP(Android5.0~Android5.0.X). In Kitkat, it could be work or not.
 
 #Clean!
-rm -rf boot.img
+rm -rf out
+make -j32 clobber
+make cyanogenmod_hammerhead_defconfig
+mkdir out
+make -j128
 
 #Build!
-mkbootimg --kernel arch/arm/boot/zImage-dtb --ramdisk ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --output boot.img
+mkbootimg --kernel arch/arm/boot/zImage-dtb --ramdisk KDP-Tools/ramdisk.img --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --output out/boot.img
