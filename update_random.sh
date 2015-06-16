5f5d48927b20ca84739986b85626c45aaecb47f8
KangDroid <hyunwoo286@gmail.com>
2015-06-16 23:08:09 +0100

    GSED
    I am using macbook because it is mac branch
    
    Change-Id: Ife050dad7e3d2009ac7fdb260f1c662d10bbca06

--- update_random.sh
+++ update_random.sh
@@ -7,9 +7,9 @@ wget -O include/trace/events/random.h https://raw.githubusercontent.com/torvalds
 wget -O drivers/char/random.c https://raw.githubusercontent.com/torvalds/linux/master/drivers/char/random.c
 wget -O lib/random32.c https://raw.githubusercontent.com/torvalds/linux/master/lib/random32.c
 
-sed -i -e 's/uapi\/linux\/random.h/linux\/uapi_random.h/g' include/linux/random.h
+gsed -i -e 's/uapi\/linux\/random.h/linux\/uapi_random.h/g' include/linux/random.h
 
-sed -i 's/[[:space:]]*$//' include/linux/uapi_random.h include/linux/random.h include/trace/events/random.h drivers/char/random.c lib/random32.c
+gsed -i 's/[[:space:]]*$//' include/linux/uapi_random.h include/linux/random.h include/trace/events/random.h drivers/char/random.c lib/random32.c
 
 # I am not making this driver "yet". Im using Macbook for managing source but not build.
 #make drivers/char/random.o lib/random32.o
